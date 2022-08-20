035d6ba82a3aa325051d9df6501ef131abb14f78
suckitfa <254202042@qq.com>
2022-08-20 17:34:58 +0800

    设置导航栏，增加动画特效

--- portfilio/js/main.js
+++ portfilio/js/main.js
@@ -1,3 +1,6 @@
 $('.menu-btn').click((e) => {
   $('.menu-btn__burger').toggleClass('open')
-})
+  $('.menu-nav').toggleClass('open')
+  $('.nav').toggleClass('open')
+  $('.menu-nav__item').toggleClass('open')
+});
